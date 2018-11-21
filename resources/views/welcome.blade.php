6b568ef551a7b3bacb559d107164cc4414c4843e
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-11-21 12:54:48 +0000

    news api v4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,102 +1,113 @@
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Clickfy</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Clickfy</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 
-    </head>
-    <body>
-      <nav>
-        <div class="nav-wrapper">
-          <a href="index.php" class="brand-logo">Clickfy</a>
-          <a href="mobile-demo" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="../login.php">Login</a></li>
-            <li><a href="../register.php">Register</a></li>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-          </ul>
-        </div>
-      </nav>
-
-      <ul class="sidenav" id="mobile-demo">
-        <li><a href="/login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
+</head>
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="index.php" class="brand-logo">Clickfy</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="../login.php">Login</a></li>
+        <li><a href="../register.php">Register</a></li>
 
       </ul>
-    <div class="row">
-    <div class="col s6 offset-s6  m6 offset-m6">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">News Feed</span>
-          <?php
+</div>
+</nav>
+
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="/login.php">Login</a></li>
+    <li><a href="register.php">Register</a></li>
+
+  </ul>
+
+
+<div class="row">
+<div class="col s6 offset-s6  m6 offset-m6">
+  <div class="card blue-grey darken-1">
+    <div class="card-content white-text">
+      <h6 class="card-title center-align">News Feed</h6>
+      <?php
 
 
 
 
-                $urlArticles = file_get_contents('https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=3eb2d5e47ad34c89ac518da077edc5e8');
-                $urlArticlesArray = json_decode($urlArticles, true);
+            $urlArticles = file_get_contents('https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=3eb2d5e47ad34c89ac518da077edc5e8');
+            $urlArticlesArray = json_decode($urlArticles, true);
 
-                 $articles = $urlArticlesArray['articles'];
-                 for($i = 0; $i < count($articles); $i++) {
-                   $sites = $urlArticlesArray['articles'][$i];
-                   $output = "<ul>";
+             $articles = $urlArticlesArray['articles'];
 
-                   echo '<img src="'.$sites['urlToImage'].'">';
-                   echo '</br>';
-                   echo '<h6 class="fgd">'.$sites['description'].'</h6>';
+             for($i = 0; $i < count($articles); $i++) {
+               $sites = $urlArticlesArray['articles'][$i];
+               $output = "<ul>";
+
+echo' <div class="card">';
+echo'  <div class="card-image">';
+  echo '<img src="'.$sites['urlToImage'].'">';
+  echo '  <span class="card-title">'.$sites['description'].'</span>';
+echo '</div>';
+  echo'<div class="card-action  deep-orange accent-1>';
+echo'<a  class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>Comment</a>';
+
+echo ' </div>';
+echo'</div>';
 
 
 
 
-
-                   }
-    ?>
-        </div>
-        <div class="card-action">
-          <a href="https://www.facebook.com">Comment</a>
-          <a href="https://twitter.com">Like</a>
-        </div>
-      </div>
+               }
+?>
     </div>
   </div>
+
+</div>
+</div>
+
+
+
 
 
 <style>
 .nav-wrapper{
-  width:100%;
+width:100%;
 }
 img {
-  width:300px;
-  height:200px;
+width:300px;
+height:200px;
 
 }
 .fgd{
-  padding:1em;
+padding:1em;
 
 }
 .ass{
-  width:50%%
-  height 40%;
-  float:right;
-  border-color: #0d39c6;
+width:50%%
+height 40%;
+float:right;
+border-color: #0d39c6;
 }
 </style>
 </body>
@@ -106,15 +117,15 @@ img {
 
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-   var elems = document.querySelectorAll('.sidenav');
-   var instances = M.Sidenav.init(elems, options);
- });
+var elems = document.querySelectorAll('.sidenav');
+var instances = M.Sidenav.init(elems, options);
+});
 
- // Or with jQuery
+// Or with jQuery
 
- $(document).ready(function(){
-   $('.sidenav').sidenav();
- });
+$(document).ready(function(){
+$('.sidenav').sidenav();
+});
 
 </script>
 
@@ -122,12 +133,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-    <!-- jQuery library -->
+<!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
 
 </html>
